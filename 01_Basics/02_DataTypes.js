fb6d672b7512c8a10cdc2cba3f3f3feca1c4838e
noob-numaann <numanulashraf04@gmail.com>
2026-08-15 06:04:04 +0000

    data types

---
+++ 01_Basics/02_DataTypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all JS cpse as newer version
+
+// alert  (3 + 3) // we are using nodejs, not browser
+
+console.log(3 + 3) // code readability should be high
+
+console.log("Numan")
+
+let name = "Numan"
+let age = 22
+let isLoggedIn = false
+let state = null  // empty
+
+// number => to power 53
+// bignit
+// string => ""
+//  boolean => true/false
+// null => standalone value
+// undefined 
+// symbol
+
+// object
+
+console.log(typeof "Numan");
+console.log(typeof age);
+console.log(typeof isLoggedIn);
+console.log(typeof null); // type object, output object.
+console.log(typeof undefined); // output will be undefined cuz it is a type  in JS.
+
