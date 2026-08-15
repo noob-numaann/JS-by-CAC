dbd6758f1f2568953cdabb7d89e61b32eb5f3489
noob-numaann <numanulashraf04@gmail.com>
2026-08-15 06:41:22 +0000

    conversion of Datatypes

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,30 @@
+ let score = "33" //  converion  "33" => 33
+// let score = "343jfi" //  => NaN
+// let score = null // => 0
+// let score = undefined  // => NaN
+// let score = true // => 1; false => 0
+// let score = "Numan"  // => NaN
+// console.log(typeof score);
+// console.log(typeof(score));
+
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+let isLoggedIn = 1 // => true
+// let isLoggedIn = 0  // => false
+// let isLoggedIn = "" // => false
+// let isLoggedIn = "Numan" // => true 
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+let someNumber = 44
+let stringNumber = String (someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
