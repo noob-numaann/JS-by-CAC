09d700e5bb37a3d32a05fd13d522dfeae67d2455
noob-numaann <numanulashraf04@gmail.com>
2026-08-18 11:03:15 +0000

    Math and Num

---
+++ 01_Basics/06_Nums_and_Maths.js
@@ -0,0 +1,37 @@
+const score = 400
+// console.log(score);
+
+const balance = new  Number(200)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const otherNumber = 444.8911
+
+// console.log(otherNumber.toPrecision(4));
+
+const hundreds = 4440000
+
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+// ++++++++++++++++++++  Maths ++++++++++++++++++++++
+
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(5.6));
+// console.log(Math.ceil(6.3));
+// console.log(Math.floor(2.9));
+// console.log(Math.min(9,8,1,6));
+// console.log(Math.max(9, 1, 5 ,  10));
+
+console.log(Math.random());
+console.log((Math.random()*10) +1);
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max -  min)) + min);
