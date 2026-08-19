512277ab9d471f772ef5081e5bbf13d7944fa06e
noob-numaann <numanulashraf04@gmail.com>
2026-08-19 17:40:56 +0000

    Arrays part 1

---
+++ 01_Basics/07_Dates.js
@@ -0,0 +1,42 @@
+// DATES
+
+let myDate = new Date()
+console.log(myDate);
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toISOString());
+console.log(myDate.toJSON());
+console.log(myDate.toLocaleDateString());
+console.log(myDate.toTimeString());
+console.log(myDate.toLocaleString());
+console.log(myDate.toUTCString());
+console.log(myDate.getTimezoneOffset());
+
+console.log(typeof myDate);
+
+// let myCreatedDate = new Date(2026, 7, 18)
+// let myCreatedDate = new Date( 2026, 0, 18 , 17, 0)
+let myCreatedDate = new Date("08-18-2026")
+// console.log(myCreatedDate.toDateString());
+
+let myTimeStamp = Date.now()
+
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date() 
+// console.log(newDate);
+// console.log(newDate.getMonth() +1);
+// console.log(newDate.getDate());
+
+console.log(newDate.toLocaleString('default', {
+    weekday:"long",
+    calendar:"islamic",
+    month:"long",
+    minute:"numeric"
+
+}));
+
+
+
