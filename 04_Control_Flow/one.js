1660db1120cc9f9ebb188ee27e9b6fd1a401b7c5
noob-numaann <numanulashraf04@gmail.com>
2026-08-26 11:08:20 +0000

    one and switch

---
+++ 04_Control_Flow/one.js
@@ -0,0 +1,75 @@
+// if
+
+// if (true){
+
+// }
+
+// if (false){
+// //  all the scope here will not get executed
+// }
+
+
+const isUserLoggedIn = true
+
+if (isUserLoggedIn){
+}
+const  noLogin = true
+// if(2 == "2"){
+//     console.log("executed");
+    
+// }
+// if(2 !== 4){
+//     console.log("executed");
+    
+// }
+
+
+// const temprature = 44
+// if (temprature === 40){
+//     console.log("less than 50");
+// } else {
+//     console.log("temprature is greater than 50");
+// }
+
+// console.log("execute");
+
+// <, >, <=, >=, ==, !>, ===, !==,
+
+// const score = 444
+//  if (score > 100){
+// const power = "fly"
+// console.log(`User power: ${power}`);
+//  }
+// console.log(`User power: ${power}`);// power is not defined
+
+
+// const balance = 1000
+
+// if(balance > 500) console.log("test");
+
+// if (balance < 500) {
+//         console.log("less than 500");       
+// } else if(balance < 750){
+//     console.log("less than 750");
+    
+// } else if (balance < 900){
+//     console.log("less than 900");
+// } else {
+//     console.log("less than 1200");
+    
+// }
+
+const userLoggedIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+
+if (userLoggedIn && debitCard && 2==2){
+    console.log("Allow to Buy Courses");
+    
+}
+if (loggedInFromGoogle || loggedInFromEmail){
+    console.log("user Logged In");
+    
+}
---
+++ 04_Control_Flow/switch.js
@@ -0,0 +1,32 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "March" // we can use numbers and  strings both.
+
+switch (month) {
+    case 1:
+        console.log("January");
+        break;
+
+    case 2:
+        console.log("Feb");
+        break;
+
+    case "March":
+        console.log("March");
+        break;
+    case "April":
+        console.log("April");
+        break;
+
+    default:
+        console.log("default case match");
+        
+        break;
+}
